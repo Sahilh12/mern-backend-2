70e8e04962f5d09a48ce27c5df9f2668ca764875
Sahilh12 <168436053+Sahilh12@users.noreply.github.com>
2024-10-08 14:41:50 +0530

    forget link

--- controller/userController.js
+++ controller/userController.js
@@ -204,7 +204,7 @@ module.exports.forgotPassword = catchAsyncError(async (req, res, next) => {
     }
     const resetToken = await user.getResetToken()
     await user.save({ validateBeforeSave: false })
-    const resetUrl = `http://localhost:5173/resetPassword/${resetToken}`
+    const resetUrl = `https://mern-backend-2-xrcy.onrender.com/resetPassword/${resetToken}`
     const message = `Your password reset link is :- \n\n ${resetUrl} \n\n if you've not request for this , ignore it.`
 
     try {
